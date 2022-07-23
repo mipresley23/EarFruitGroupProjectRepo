6b69ab233557399bdab023f9effc0670112d77de
Jesse <jnjoroge13@gmail.com>
2022-07-23 01:22:54 -0700

    added spotify background colors

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import SearchBar from "./SearchBar/SearchBar";
+import SearchBar from "../SearchBar/SearchBar";
+import './HomePage.css'
 
 function HomePage() {
 
 	return (
-		<div>
+		<div className="home-container">
 			<h1>Home</h1>
 			<SearchBar/>
 		</div>
