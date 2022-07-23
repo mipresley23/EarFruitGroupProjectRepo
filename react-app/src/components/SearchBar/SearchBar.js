1630bd04591001b90ce70407322e234b419a164b
Jesse <jnjoroge13@gmail.com>
2022-07-23 00:40:35 -0700

    playlist search css

--- react-app/src/components/SearchBar/SearchBar.js
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -1,36 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import './SearchBar.css'
+import "./SearchBar.css";
 
-export default function SearchBar(){
-    const [search, setSearch] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
-
+export default function SearchBar() {
+	const [search, setSearch] = useState("");
+	const [searchResults, setSearchResults] = useState([]);
 
 	async function onSubmit(e) {
 		e.preventDefault();
 		// console.log(search);
 		async function fetchData() {
 			const response = await fetch(`/api/playlists/${search}`);
-            const responseData = await response.json();
-            // console.log(responseData.playlists)
-            setSearchResults(responseData.playlists)
+			const responseData = await response.json();
+			// console.log(responseData.playlists)
+			setSearchResults(responseData.playlists);
 		}
 		fetchData();
-    }
+	}
 
+	const playlistSearchResults = searchResults.map((playlist) => {
+		console.log(playlist);
+		return (
+			<div className="playlist-card" key={playlist.name}>
+				<div className="playlist-card-contents">
+					<img src={playlist.cover_img_url} />
+					<p id="playlist-name">{playlist.name}</p>
+					<p>By {playlist.user.username}</p>
+				</div>
+			</div>
+		);
+	});
 
-    const playlistSearchResults = searchResults.map((playlist) => {
-        // console.log(playlist.name)
-        return (
-            <div key={playlist.name}>
-                <img src={playlist.cover_img_url} width='200'/>
-                <p>{playlist.name}</p>
-            </div>
-        )
-    })
-
-    return (
+	return (
 		<div>
 			<form onSubmit={onSubmit}>
 				<input
@@ -41,8 +42,12 @@ export default function SearchBar(){
 				/>
 				<button>Search</button>
 			</form>
-			<h1>Search Results: </h1>
-            <div>{playlistSearchResults}</div>
+			{searchResults.length>1 && (
+				<>
+					<h2>Playlists: </h2>
+					<div className="playlists-container">{playlistSearchResults}</div>
+				</>
+			)}
 		</div>
 	);
 }
