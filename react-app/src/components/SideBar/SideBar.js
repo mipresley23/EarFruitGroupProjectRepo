335edb036b8799e4ffe7f59709cea592cc2be325
Jesse <jnjoroge13@gmail.com>
2022-07-24 01:53:01 -0700

    nav and sidebar css complete

--- react-app/src/components/SideBar/SideBar.js
+++ react-app/src/components/SideBar/SideBar.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import './SideBar.css'
 export default function SideBar() {
-
-
     return (
         <div className='side-bar-container'>
             <h2>EarFruit</h2>
-            <NavLink to='/' exact={true} >
-
+            <NavLink className='sidebar-link' to='/' exact={true} >
+                <i class="fa fa-home"></i>
                 Home
             </NavLink>
-            <NavLink to='/search' exact={true} >
+            <NavLink className='sidebar-link' to='/search' exact={true} >
                 <i class="fa fa-search"></i>
                 Search
             </NavLink>
