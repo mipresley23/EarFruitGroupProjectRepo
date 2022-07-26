8e47d7076b92ab77521972cc4462112ce70d1aa8
Jesse <jnjoroge13@gmail.com>
2022-07-26 13:59:14 -0700

    auth

--- react-app/src/components/SideBar/SideBar.js
+++ react-app/src/components/SideBar/SideBar.js
@@ -6,9 +6,9 @@ import "./SideBar.css";
 
 export default function SideBar() {
 	const dispatch = useDispatch();
-	const history = useHistory()
+	const history = useHistory();
 	const [playlists, setPlaylists] = useState([]);
-	const [myPlaylistNumber, setMyPlaylistNumber] = useState(1)
+	const [myPlaylistNumber, setMyPlaylistNumber] = useState(1);
 
 	const playlistsSelector = useSelector((state) => state.playlists);
 	const sessionUser = useSelector((state) => state.session.user);
@@ -27,35 +27,38 @@ export default function SideBar() {
 			playlists.filter((playlist) => playlist.user.id === sessionUser.id);
 	}
 
-	const userPlaylistNameArray = []
-	usersPlaylists?.map(playlists=>userPlaylistNameArray.push(playlists.name))
+	const userPlaylistNameArray = [];
+	usersPlaylists?.map((playlists) =>
+		userPlaylistNameArray.push(playlists.name)
+	);
 	// console.log(userPlaylistNameArray)
-	let i = 1
+	let i = 1;
 	// console.log(userPlaylistNameArray.includes(`My Playlist #${i}`))
 	while (userPlaylistNameArray.includes(`My Playlist #${i}`)) {
-
-		i++
+		i++;
 	}
 	useEffect(() => {
-		setMyPlaylistNumber(i)
-	},[i])
+		setMyPlaylistNumber(i);
+	}, [i]);
 	const onSubmit = async (e) => {
+		if (!sessionUser) {
+			history.push("/login");
+		}
 		if (sessionUser) {
-			e.preventDefault()
-			setMyPlaylistNumber(i)
+			e.preventDefault();
+			setMyPlaylistNumber(i);
 			// console.log(myPlaylistNumber)
 			const playlist = {
-				name:`My Playlist #${myPlaylistNumber}`,
-				description:`${sessionUser.username}'s Playlist`,
-				cover_img_url:''
-			}
-			const newPlaylist = await dispatch(thunkAddPlaylist(playlist))
+				name: `My Playlist #${myPlaylistNumber}`,
+				description: `${sessionUser.username}'s Playlist`,
+				cover_img_url: "",
+			};
+			const newPlaylist = await dispatch(thunkAddPlaylist(playlist));
 			if (newPlaylist) {
-				history.push(`/playlists/${newPlaylist.id}`)
+				history.push(`/playlists/${newPlaylist.id}`);
 			}
 		}
-    }
-	if (!playlists) return null;
+	};
 	return (
 		<div className="side-bar-container">
 			{/* <h2>EarFruit</h2> */}
@@ -90,10 +93,14 @@ export default function SideBar() {
 			<div className="side-bar-playlist-list">
 				{sessionUser &&
 					usersPlaylists.map((playlist) => (
-						<NavLink className="sidebar-link sidebar-playlist-link" to={`/playlists/${playlist.id}`}>{playlist.name}</NavLink>
+						<NavLink
+							className="sidebar-link sidebar-playlist-link"
+							to={`/playlists/${playlist.id}`}
+						>
+							{playlist.name}
+						</NavLink>
 					))}
 			</div>
 		</div>
 	);
-
 }
