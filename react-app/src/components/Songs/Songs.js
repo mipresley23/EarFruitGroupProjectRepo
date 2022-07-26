e812518d3def492f78b123dae8ce74a9d30d18fe
Michael Presley <mipresley23@gmail.com>
2022-07-26 19:40:51 -0400

    testing music player functionality

--- react-app/src/components/Songs/Songs.js
+++ react-app/src/components/Songs/Songs.js
@@ -1,27 +1,62 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import AudioListProvider, {AudioListContext} from '../../context/audioList';
 import { getSongs } from '../../store/songs';
 import EditSongModal from '../EditSongModal';
 import ConfirmDeleteModal from '../ConfirmDeleteModal';
+import ReactJkMusicPlayer from 'react-jinke-music-player'
 
 function Songs() {
+
+
 
     const songs = useSelector(state => state?.songs);
     const dispatch = useDispatch();
     const history = useHistory();
     const songsArr = Object.values(songs);
+    console.log('songs: ', songs)
+    const {audioList, setAudioList} = useContext(AudioListContext)
+    const [playButton, setPlayButton] = useState(false);
+
+    const [audios, setAudios] = useState([])
+    console.log('audios: ', audios)
 
     useEffect(() => {
         dispatch(getSongs());
     }, [dispatch]);
 
+    useEffect(() => {
+        setAudios(Object.values(songs))
+    }, [songs])
+
+    const handlePlaySong = async(e) => {
+        e.preventDefault();
+        const audioArr = e.target.value.split(',')
+        console.log('audioArr: ', audioArr)
+        setAudioList([])
+        await setAudioList([{name: audioArr[0], artist: audioArr[1], musicSrc: audioArr[2]}])
+    }
+    const handleAddToQueue = async(e) => {
+        e.preventDefault();
+        const audioArr = e.target.value.split(',')
+        console.log('audioArr: ', audioArr)
+        console.log(audioList !== null)
+        if(audioList !== null){
+            setAudioList([...audioList], [{name: audioArr[0], artist: audioArr[1], musicSrc: audioArr[2]}])
+        }
+        setAudioList([{name: audioArr[0], artist: audioArr[1], musicSrc: audioArr[2]}])
+    }
+
+    console.log('audioList:', audioList)
     return (
         <div>
             <NavLink to='/add-song' >Add Song</NavLink>
             <ul>
                 {songsArr.map(song => (
+                    <div>
+
                     <li key={song.id}>
                         <EditSongModal songId={song?.id} onClick={console.log('click!')}/>
                         <ConfirmDeleteModal songId={song?.id} onClick={console.log('click!')}/>
@@ -30,11 +65,16 @@ function Songs() {
                         <p>{song?.album}</p>
                         <p>{song?.genre}</p>
                         <p>{song?.source}</p>
+                        <button value={[song.name, song.artist, song.source]} type='button' onClick={handlePlaySong}>play</button>
+                        <button value={[song.name, song.artist, song.source]} type='button' onClick={handleAddToQueue}>Add to Queue</button>
                     </li>
+                    </div>
                 ))}
             </ul>
         </div>
     )
 }
+
+// (e) => console.log(e.target.value)
 
 export default Songs;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -7,6 +7,7 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { ModalProvider } from './context/Modal';
+import AudioListProvider from './context/audioList';
 
 const store = configureStore();
 
@@ -14,12 +15,14 @@ const options = {theme: 'dark'}
 
 ReactDOM.render(
   <React.StrictMode>
+    <AudioListProvider>
     <Provider store={store}>
       <ModalProvider>
         <App />
         {/* <ReactJkMusicPlayer {...options} />, */}
       </ModalProvider>
     </Provider>
+    </AudioListProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
