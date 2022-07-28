6261c03e3abae8ba99758c018353f78776710073
Michael Presley <mipresley23@gmail.com>
2022-07-28 11:30:05 -0400

    consolelogs

--- react-app/src/components/SongForm.js
+++ react-app/src/components/SongForm.js
@@ -53,7 +53,7 @@ const SongForm = () => {
         if (res.ok) {
             const jsonRes = await res.json();
             setMP3Loading(false);
-            // console.log('------jsonRes----', jsonRes.source)
+            console.log('------jsonRes----', jsonRes.source)
 
             const song = {
                 name,
@@ -63,19 +63,19 @@ const SongForm = () => {
                 source: jsonRes.source
             };
 
-            // console.log('------song------', song)
+            console.log('------song------', song)
             const response = await dispatch(createSong(song));
 
             if (response === 'Song Uploaded') {
                 history.push('/');
             }
-            
+
         }
         else {
             setMP3Loading(false);
             errors.push('The file must be an mp3');
             setErrors(errors);
-            // console.log("---error uploading song----", res)
+            console.log("---error uploading song----", res)
         }
     }
 
