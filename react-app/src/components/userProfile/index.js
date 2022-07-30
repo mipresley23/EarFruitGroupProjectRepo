da625971e8ec60cf75fb84014587a6ad31c86239
Jesse <jnjoroge13@gmail.com>
2022-07-30 15:07:16 -0700

    user profile bug fix

--- react-app/src/components/userProfile/index.js
+++ react-app/src/components/userProfile/index.js
@@ -1,17 +1,17 @@
-
-import React, { useState, useEffect, useContext } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useParams } from 'react-router-dom';
-import AudioListProvider, {AudioListContext} from '../../context/audioList';
-import { getUsersThunk, editUserThunk } from '../../store/Users';
-import { thunkGetPlaylists } from '../../store/playlists';
-import { getSongs } from '../../store/songs';
-import playButton from '../assets/play_button.png';
-import addToPlaylistButton from '../assets/addtoPlaylist.png';
-import circleLogo from '../assets/earfruit-kiwi-circle-logo.png';
+import React, { useState, useEffect, useContext } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useParams } from "react-router-dom";
+import AudioListProvider, { AudioListContext } from "../../context/audioList";
+import { getUsersThunk, editUserThunk } from "../../store/Users";
+import { thunkGetPlaylists } from "../../store/playlists";
+import { getSongs } from "../../store/songs";
+import EditSongModal from "../EditSongModal";
+import ConfirmDeleteModal from "../ConfirmDeleteModal";
+import playButton from "../assets/play_button.png";
+import addToPlaylistButton from "../assets/addtoPlaylist.png";
+import circleLogo from "../assets/earfruit-kiwi-circle-logo.png";
 import defaultPlaylistImage from "../assets/my-playlist-img.png";
-import './userProfile.css'
-
+import "./userProfile.css";
 
 const UserProfile = () => {
 	const dispatch = useDispatch();
@@ -95,47 +95,6 @@ const UserProfile = () => {
 		}
 	};
 
-
-
-  if (!user) return null;
-  return (
-    <div>
-      <div id='user-profile-header'>
-        <h1>{user && user.username}</h1>
-        <img id='user-profile-image' src={user && user.photo_url} alt='user image' />
-      </div>
-      <div id='user-profile-playlists-container'>
-        <h2>{user.username}'s Playlists</h2>
-        <div id='all-users-playlist-container'>
-          {
-            thisUsersPlaylists && thisUsersPlaylists.map(playlist => (
-              <div>
-                <NavLink to={`/playlists/${playlist.id}`}>
-                  <div className='profile-playlists-container'>
-                    <img className='profile-playlist-cover-art' src={playlist.cover_img_url ? playlist.cover_img_url : defaultPlaylistImage} alt='album cover' />
-                    <p id='playlist-nav-link-text'>{playlist.name}</p>
-                  </div>
-                </NavLink>
-              </div>
-            ))
-          }
-        </div>
-        <div id='users-songs-container'>
-          <h2 id='users-songs-header'>{user.username}'s Songs</h2>
-          <ol>
-
-          {
-            thisUsersSongs && thisUsersSongs.map(song => (
-              <div id='users-songs-list'>
-                <div id='song-info'>
-                  <li>
-                    <p id='song-title'>{song.name}</p>
-                    <p id='song-artist'>{song.artist}</p>
-                  </li>
-                </div>
-                <div id='song-buttons-container'>
-                <i id="profile-play-button"
-
 	let songNum = 0;
 	const songList = thisUsersSongs.map((song) => {
 		songNum++;
@@ -149,7 +108,6 @@ const UserProfile = () => {
 				<td className="search-song-album">{song.album}</td>
 				<td className="search-song-button-cont">
 					<i
-
 						onClick={() =>
 							handlePlaySong([
 								song.name,
@@ -186,60 +144,26 @@ const UserProfile = () => {
 							<div>
 								<NavLink to={`/playlists/${playlist.id}`}>
 									<div className="profile-playlists-container">
-										<img
-											className="profile-playlist-cover-art"
-											src={playlist.cover_img_url}
-											alt="album cover"
-										/>
+                  <img className='profile-playlist-cover-art' src={playlist.cover_img_url ? playlist.cover_img_url : defaultPlaylistImage} alt='album cover' />
 										<p id="playlist-nav-link-text">{playlist.name}</p>
 									</div>
 								</NavLink>
 							</div>
 						))}
-        </div>
-        <div className="song-list-cont">
-						<table className="search-song-table">
-							<thead>
-								<tr className="border-white">
-									<th className="search-song-number">#</th>
-									<th className="">Title</th>
-									<th className="">Album</th>
-									<th className=""></th>
-								</tr>
-							</thead>
-							<tbody>{songList}</tbody>
-						</table>
-					</div>
-				{/* <div id="users-songs-container">
-					<h2 id="users-songs-header">{user.username}'s Songs</h2>
-					<ol>
-						{thisUsersSongs &&
-							thisUsersSongs.map((song) => (
-								<div id="users-songs-list">
-									<div id="song-info">
-										<li>
-											<p id="song-title">{song.name}</p>
-											<p id="song-artist">{song.artist}</p>
-										</li>
-									</div>
-									<div id="song-buttons-container">
-										<i
-											id="profile-play-button"
-											onClick={() =>
-												handlePlaySong([
-													song.name,
-													song.artist,
-													song.source,
-													song.albumImgUrl,
-												])
-											}
-											class="splash-song-button fa-solid fa-play fa-lg"
-										/>
-									</div>
-								</div>
-							))}
-					</ol>
-				</div> */}
+				</div>
+				<div className="song-list-cont">
+					<table className="search-song-table">
+						<thead>
+							<tr className="border-white">
+								<th className="search-song-number">#</th>
+								<th className="">Title</th>
+								<th className="">Album</th>
+								<th className=""></th>
+							</tr>
+						</thead>
+						<tbody>{songList}</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
 	);
