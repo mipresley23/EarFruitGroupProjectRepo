446e3399060b8fff832e19861698ec0e550f2152
Michael Presley <mipresley23@gmail.com>
2022-08-01 11:36:16 -0400

    profileimg

--- react-app/src/components/userProfile/index.js
+++ react-app/src/components/userProfile/index.js
@@ -127,7 +127,7 @@ const UserProfile = () => {
 				<h1>{user && user.username}</h1>
 				<img
 					id="user-profile-image"
-					src={user && user.photo_url}
+					src={user.photo_url ? user.photo_url : "https://as1.ftcdn.net/jpg/03/46/83/96/240_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"}
 					alt="user image"
 				/>
 			</div>
